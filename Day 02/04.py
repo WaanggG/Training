8a54a28d08f1ae8deab93559151dcae57e938a59
WaanggG <ducquang06032003@gmail.com>
2022-08-25 11:00:24 +0700

    add

---
+++ Day 02/04.py
@@ -0,0 +1,13 @@
+# tinh so tuoi con lai
+age = input("What is your current age? ")
+
+age_as_int = int(age)
+
+years_remaining = 90 - age_as_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+message = f"You have {days_remaining} days, {weeks_remaining} weeks, {months_remaining} months, {years_remaining} years!! "
+print(message)
+
