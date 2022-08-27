c2623f8ad21e2e779e9f429cc749e7667d056d62
WaanggG <ducquang06032003@gmail.com>
2022-08-27 17:57:53 +0700

    up

---
+++ Day 03/3.4_pizza_order.py
@@ -0,0 +1,5 @@
+print("Welcome to Python Pizza Deliveries!")
+size = inpit("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you wnat pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
--- Day 03/_rollercoaster.py
+++ Day 03/_rollercoaster.py
@@ -1,15 +1,26 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 age = int(input("What is your age ?"))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     if age < 12 :
+        bill = 5
         print("Please pay $5 ")
     elif age <= 18:
+        bill = 7
         print("Please pay $7 ")
     else:
+        bill = 12
         print("Please pay 12$ ")
+    want_photo = input("Do you want a photo taken? Y or N. ")
+    if want_photo == "Y":
+        bill += 3
+        print(f"Your final bill is {bill}")
+
+    else:
+       print(f"Your final bill is {bill}")
 else:
     print("Sorry, you have to grow taller before you can ride.")
 
