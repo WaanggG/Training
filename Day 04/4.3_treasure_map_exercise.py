8daa76603e77961a81b5bd46753b8ee210a1d213
WaanggG <ducquang06032003@gmail.com>
2022-09-04 21:57:22 +0700

    up code

---
+++ Day 04/4.3_treasure_map_exercise.py
@@ -0,0 +1,18 @@
+
+row1 = ['⬜️', '⬜️', '⬜️']
+row2 = ['⬜️', '⬜️', '⬜️']
+row3 = ['⬜️', '⬜️', '⬜️']
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}\n")
+position = input("Where do you want to put the treasure? ")
+
+horizonal = int(position[0])
+ vertical = int(position[1])
+
+selected_row = map[vertical - 1]
+selected_row[horizonal - 1] = "X"
+
+# map[horizonal - 1][horizonal - 1 ]= "X"
+
+# print(map[vertical - 1])
+print(f"{row1}\n{row2}\n{row3}\n")
