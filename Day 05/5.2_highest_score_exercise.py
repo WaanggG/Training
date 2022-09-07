f27cfcf838810451fd06c37964bc43270d24927f
WaanggG <ducquang06032003@gmail.com>
2022-09-07 20:57:42 +0700

    up code

--- Day 05/5.2_highest_score_exercise.py
+++ Day 05/5.2_highest_score_exercise.py
@@ -1,7 +1,15 @@
 
-student_scores = input("input a list of student score: ")
+student_scores = input("input a list of student score: ").split()
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 print(student_scores)
 
-# print(max(student_scores))
+print(max(student_scores))
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score in the class is {highest_score}")
+
+
--- Day 05/_loops.py
+++ Day 05/_loops.py
@@ -1,5 +1,11 @@
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit + "Pie")
-print(fruits)
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + "Pie")
+# print(fruits)
+
+
+total = 0
+for number in range(1,10):
+    total += number
+print(total)
