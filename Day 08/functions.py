91be6d0495729880551b8fec4aae362fdf9291b2
WaanggG <ducquang06032003@gmail.com>
2022-09-16 00:11:09 +0700

    up code

---
+++ Day 08/functions.py
@@ -0,0 +1,11 @@
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Quang")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Quang", "Nowhere")
