b4812c4db54aab2cd2e020d4e95f5febce3c5bd0
WaanggG <ducquang06032003@gmail.com>
2022-09-17 12:38:55 +0700

    up code

--- Day 08/caesar-cipher.py
+++ Day 08/caesar-cipher.py
@@ -1,24 +1,45 @@
 
-from turtle import position
-
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrpt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-    for letter in plain_text:
+def caesar(strat_text, shift_amount, cipher_direction):
+    end_text = ""
+    for letter in strat_text:
         position = alphabet.index(letter)
+        if cipher_direction == "decode":
+            shift_amount *= -1
         new_position = position + shift_amount
-        # new_letter = alphabet[new_position]
-        cipher_text += alphabet[new_position]
-    print("The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
+    print(f"Here is the{direction}d result:{end_text}")
 
-encrypt(plain_text = text, shift_amount = shift)
+caesar(start_text = text, shift_amount= shift, cipher_direction= direction)
 
-def encrypt(cipher_text, shift_amount):
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
+
+
+
+
+# def encrypt(plain_text, shift_amount):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         # new_letter = alphabet[new_position]
+#         cipher_text += alphabet[new_position]
+#     print(f"The encoded text is {cipher_text}")
+
+# def decrypt(cipher_text, shift_amount):
+#     plain_text = ""
+#     for letter in cipher_text:
+#         position = alphabet.index(letter)
+#         new_position = position - shift_amount
+#         plain_text += alphabet[new_position]
+#     print(f"The decode text is {plain_text}")
+
+# if direction == "encode":
+#     encrypt(plain_text = text, shift_amount = shift)
+# elif direction == "decode":
+#     decrypt(cipher_text = text, shift_amount = shift)
+
